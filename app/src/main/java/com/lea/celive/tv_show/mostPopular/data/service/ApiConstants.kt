e715cee3056fcc798511060ac6cdee7835b64239
Léa <pedra@et.intechinfo.fr>
2024-11-20 11:36:18 +0100

    init

---
+++ app/src/main/java/com/lea/celive/tv_show/mostPopular/data/service/ApiConstants.kt
@@ -0,0 +1,6 @@
+package com.lea.celive.tv_show.mostPopular.data.service
+
+object ApiConstants {
+    const val BASE_URL = "https://www.episodate.com/api/most-popular?page=1"
+    const val PAGE = "page"
+}
